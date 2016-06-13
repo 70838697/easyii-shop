4b35b7bd0a427d3995b632143d6134563c2abc5b
shpchen <shpchen@ustc.edu.cn>
2016-06-13 17:29:13 +0800

    easyyii language package for simplyfied Chinese.

---
+++ mine/zh/install.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'Installation' => '安装',
+    'Install' => '安装',
+    'Root password' => 'Root的密码',
+    'Admin E-mail' => 'Admin的E-mail',
+    'Robot E-mail' => 'Root的E-mail',
+    'Auth time' => '授权时间',
+    'Frontend toolbar position' => '前台工具条位置',
+    'Password to login as root' => 'root的密码',
+    'Used as "ReplyTo" in mail messages' => '邮件中回复到(ReplyTo)的邮箱',
+    'Used as "From" in mail messages' => '邮件中发信(From)的邮箱',
+    'Required for using captcha in forms (guestbook, feedback)' => '如需在(留言本, 意见反馈)等使用验证码时需要填写',
+    'You easily can get keys on' => '你能便捷地获取一个',
+    'ReCaptcha website' => '验证码网站',
+    'Installation completed' => '安装完成',
+    'Installation error' => '安装错误',
+    'Cannot connect to database. Please configure `{0}`.' => '不能连接数据库，请配置 `{0}`.',
+    'EasyiiCMS is already installed. If you want to reinstall easyiiCMS, please drop all tables with prefix `easyii_` from your database manually.' => 'EasyiiCMS已被安装，如果您想重装的话，请删除数据库中所有`easyii_`开头的表.'
+];
