6944375acb9eb8fd52456205c880ae690ef1b147
shpchen <shpchen@ustc.edu.cn>
2016-06-21 15:08:55 +0800

    yii2 audit not ready yet.

--- app/config/web.php
+++ app/config/web.php
@@ -12,6 +12,45 @@ $config = [
     'language' => 'zh-CN',
     'runtimePath' => $webroot . '/runtime',
     'vendorPath' => $webroot . '/vendor',
+	'modules' => [
+        'audit' => [
+            'class' => 'bedezign\yii2\audit\Audit',
+            // the layout that should be applied for views within this module
+            'layout' => 'main',
+            // Name of the component to use for database access
+            'db' => 'db', 
+            // List of actions to track. '*' is allowed as the last character to use as wildcard
+            'trackActions' => ['*'], 
+            // Actions to ignore. '*' is allowed as the last character to use as wildcard (eg 'debug/*')
+            'ignoreActions' => ['audit/*', 'debug/*'],
+            // Maximum age (in days) of the audit entries before they are truncated
+            'maxAge' => 'debug',
+            // IP address or list of IP addresses with access to the viewer, null for everyone (if the IP matches)
+            'accessIps' => [], 
+            // Role or list of roles with access to the viewer, null for everyone (if the user matches)
+            //'accessRoles' => ['admin'],
+            // User ID or list of user IDs with access to the viewer, null for everyone (if the role matches)
+            //'accessUsers' => [],
+            // Compress extra data generated or just keep in text? For people who don't like binary data in the DB
+            'compressData' => false,
+            // The callback to use to convert a user id into an identifier (username, email, ...). Can also be html.
+            //'userIdentifierCallback' => ['app\models\User', 'userIdentifierCallback'],
+            // If the value is a simple string, it is the identifier of an internal to activate (with default settings)
+            // If the entry is a '<key>' => '<string>|<array>' it is a new panel. It can optionally override a core panel or add a new one.
+            /*'panels' => [
+                'audit/request',
+                'audit/error',
+                'audit/trail',
+                'app/views' => [
+                    'class' => 'app\panels\ViewsPanel',
+                    // ...
+                ],
+            ],*/
+            'panelsMerge' => [
+               // ... merge data (see below)
+            ]
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
